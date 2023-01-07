dbbc39b1a216925e18ffa4dc39bd4f4b25e8326f
Elzhas <103108579+ElzhasK@users.noreply.github.com>
2023-01-07 16:42:46 +0600

    some informations about our 6 random cities

---
+++ RandomCity/script.js
@@ -0,0 +1,69 @@
+function Menu1() {
+  document.getElementById("almaty").style.display = "block";
+  document.getElementById("astana").style.display = "none";
+  document.getElementById("shymkent").style.display = "none";
+  document.getElementById("aktau").style.display = "none";
+  document.getElementById("oskemen").style.display = "none";
+
+  document.getElementById("div1").style.display = "block";
+  document.getElementById("div2").style.display = "none";
+  document.getElementById("div3").style.display = "none";
+  document.getElementById("div4").style.display = "none";
+  document.getElementById("div5").style.display = "none";
+}
+
+function Menu2() {
+  document.getElementById("almaty").style.display = "none";
+  document.getElementById("astana").style.display = "block";
+  document.getElementById("shymkent").style.display = "none";
+  document.getElementById("aktau").style.display = "none";
+  document.getElementById("oskemen").style.display = "none";
+
+  document.getElementById("div1").style.display = "none";
+  document.getElementById("div2").style.display = "block";
+  document.getElementById("div3").style.display = "none";
+  document.getElementById("div4").style.display = "none";
+  document.getElementById("div5").style.display = "none";
+}
+
+function Menu3() {
+  document.getElementById("almaty").style.display = "none";
+  document.getElementById("astana").style.display = "none";
+  document.getElementById("shymkent").style.display = "block";
+  document.getElementById("aktau").style.display = "none";
+  document.getElementById("oskemen").style.display = "none";
+
+  document.getElementById("div1").style.display = "none";
+  document.getElementById("div2").style.display = "none";
+  document.getElementById("div3").style.display = "block";
+  document.getElementById("div4").style.display = "none";
+  document.getElementById("div5").style.display = "none";
+}
+
+function Menu4() {
+  document.getElementById("almaty").style.display = "none";
+  document.getElementById("astana").style.display = "none";
+  document.getElementById("shymkent").style.display = "none";
+  document.getElementById("aktau").style.display = "block";
+  document.getElementById("oskemen").style.display = "none";
+
+  document.getElementById("div1").style.display = "none";
+  document.getElementById("div2").style.display = "none";
+  document.getElementById("div3").style.display = "none";
+  document.getElementById("div4").style.display = "block";
+  document.getElementById("div5").style.display = "none";
+}
+
+function Menu5() {
+  document.getElementById("almaty").style.display = "none";
+  document.getElementById("astana").style.display = "none";
+  document.getElementById("shymkent").style.display = "";
+  document.getElementById("aktau").style.display = "none";
+  document.getElementById("oskemen").style.display = "block";
+
+  document.getElementById("div1").style.display = "none";
+  document.getElementById("div2").style.display = "none";
+  document.getElementById("div3").style.display = "none";
+  document.getElementById("div4").style.display = "none";
+  document.getElementById("div5").style.display = "block";
+}
